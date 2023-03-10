87a0e09ab09d686d25d689bee071b3b7bc03092d
akalibekova <dairako2000@gmail.com>
2023-03-10 16:26:17 +0600

    fix

--- src/test/java/tests/android/browserstackTests/helpers/Browserstack.java
+++ src/test/java/tests/android/browserstackTests/helpers/Browserstack.java
@@ -1,17 +1,23 @@
 package tests.android.browserstackTests.helpers;
 
+import org.aeonbits.owner.ConfigFactory;
+import tests.android.browserstackTests.config.Config;
+
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 import static tests.android.browserstackTests.helpers.CustomApiListener.withCustomTemplates;
 
 public class Browserstack {
+    static Config config = ConfigFactory.create(Config.class);
+
     public static String getVideoUrl(String sessionId) {
+
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
                 .log().all()
                 .filter(withCustomTemplates())
-                .auth().basic("akerkekalibekova_hF0PYC", "wqWPhAJyRLLhCYHyqrMV")
+                .auth().basic(config.login(), config.password())
                 .when()
                 .get(url)
                 .then()
